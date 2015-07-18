ca1d40a924c07ef0039b62b3bef9d4b50888a678
Qinhu Wang <qinhu.wang@icloud.com>
2015-07-18 23:27:34 +0800

    imprve structure classification

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-perl ./structure_bias.pl $1 > stem-loop.txt
-echo -n "Loop:" >> stats.txt
-grep "Loop" stem-loop.txt | wc -l >> stats.txt
-echo -n "Stem:" >> stats.txt
-grep "Stem" stem-loop.txt | wc -l >> stats.txt
+# make output directory
+if [ ! -d "output" ]; then
+	mkdir output
+fi
+
+# judge RNA secondary structure
+perl ./structure_bias.pl $1 > output/$2.sl.txt
+
+# statistics
+echo -n "loop:" >> output/$2.stats.txt
+grep "loop" output/$2.sl.txt | wc -l >> output/$2.stats.txt
+echo -n "bulge:" >> output/$2.stats.txt
+grep "bulge" output/$2.sl.txt | wc -l >> output/$2.stats.txt
+echo -n "free:" >> output/$2.stats.txt
+grep "free" output/$2.sl.txt | wc -l >> output/$2.stats.txt
+echo -n "stem:" >> output/$2.stats.txt
+grep "stem" output/$2.sl.txt | wc -l >> output/$2.stats.txt
+echo -n "nosl:" >> output/$2.stats.txt
+grep "nosl" output/$2.sl.txt | wc -l >> output/$2.stats.txt
--- structure_bias.pl
+++ structure_bias.pl
@@ -18,7 +18,7 @@ use strict;
 use warnings;
 use utf8;
 
-my $seq = $ARGV[0] || "Sex8d.flank30.fa"; # sequences file
+my $seq = $ARGV[0];                       # sequences file
 my $flank = $ARGV[1] || 30;               # flank length
 my $seq_id = undef;
 my @seq_id = ();
@@ -40,15 +40,48 @@ close FASTA;
 my $str = undef;
 my $mfe = undef;
 foreach my $seq_id (@seq_id) {
+	# Predict RNA secondary structure
 	$str = `echo $seq{$seq_id} | RNAfold --noPS`;
+	# If a valid RNAfold out found
 	if ($str =~ /\(\.+\)\S*\s+\(\s*(\S+)\)/) {
+		# Capture MFE
 		$mfe = $1;
-		if ($str =~ /\S+\n[\.\(\)]{$flank}\.[\.\(\)]{$flank}\s+\(\s*\S+\)/) {
-			print "$seq_id\tLoop\t$mfe\n";
+		# If edit site locate on an unmatched region
+		if ($str =~ /\S+\n([\.\(\)]{$flank})\.([\.\(\)]{$flank})\s+\(\s*\S+\)/) {
+			# Caputre left and right parts respectively
+			my $left = $1;
+			my $right = $2;
+			# Remove dots
+			$left =~ s/\.//g;
+			$right =~ s/\.//g;
+			# If one of them is empty
+			if (length($left) == 0 or length($right) == 0) {
+				print "$seq_id\tfree\t$mfe\n";
+			} else {
+				# Extract the last and first matches
+				my $l = substr($left, length($left) - 1, 1);
+				my $r = substr($right, 1, 1);
+				# Judge structure: loop/bulge/free
+				if ($l eq "("  && $r eq "(")  {
+					print "$seq_id\tbulge\t$mfe\n";
+				} elsif ($l eq ")" && $r eq ")") {
+					print "$seq_id\tbulge\t$mfe\n";
+				} elsif ($l eq "(" && $r eq ")") {
+					print "$seq_id\tloop\t$mfe\n";
+				} elsif ($l eq ")" && $r eq "(") {
+					print "$seq_id\tfree\t$mfe\n";
+				} else {
+					warn "Unknown RNA secondary structure found in $seq_id!\n";
+				}
+			}
+		# If edit site locate on a matched region
 		} else {
-			print "$seq_id\tStem\t$mfe\n";
+			print "$seq_id\tstem\t$mfe\n";
 		}
+	# If no stem-loop found
 	} else {
-		print "$seq_id\tNoSL\t-\n";
+		$str =~ /\s+\(\s*(\S+)\)/;
+		$mfe = $1;
+		print "$seq_id\tnosl\t$mfe\n";
 	}
 }
