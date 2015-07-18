521b08242b130a1915a158b012ae13a05d667964
Qinhu Wang <qinhu.wang@icloud.com>
2015-07-18 16:45:03 +0800

    improve input

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-perl ./structure_bias.pl > stem-loop.txt
+perl ./structure_bias.pl $1 > stem-loop.txt
 echo -n "Loop:" >> stats.txt
 grep "Loop" stem-loop.txt | wc -l >> stats.txt
 echo -n "Stem:" >> stats.txt
