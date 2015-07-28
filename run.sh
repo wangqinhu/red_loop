b96dc1f1e5f8bea2c41dc0d670a7269b761ae1df
Qinhu Wang <qinhu.wang@icloud.com>
2015-07-28 09:24:43 +0800

    use forgi as structure parser

--- run.sh
+++ run.sh
@@ -6,16 +6,16 @@ if [ ! -d "output" ]; then
 fi
 
 # judge RNA secondary structure
-perl ./structure_bias.pl $1 > output/$2.sl.txt
+perl ./structure_bias.pl $1 30 > output/$2.str.txt
 
 # statistics
-echo -n "loop:" >> output/$2.stats.txt
-grep "loop" output/$2.sl.txt | wc -l >> output/$2.stats.txt
-echo -n "bulge:" >> output/$2.stats.txt
-grep "bulge" output/$2.sl.txt | wc -l >> output/$2.stats.txt
-echo -n "free:" >> output/$2.stats.txt
-grep "free" output/$2.sl.txt | wc -l >> output/$2.stats.txt
+echo -n "hairpin_loop:" >> output/$2.stats.txt
+grep "h" output/$2.str.txt | wc -l >> output/$2.stats.txt
+echo -n "interior_loop:" >> output/$2.stats.txt
+grep "i" output/$2.str.txt | wc -l >> output/$2.stats.txt
+echo -n "multi_loop" >> output/$2.stats.txt
+grep "m" output/$2.str.txt | wc -l >> output/$2.stats.txt
 echo -n "stem:" >> output/$2.stats.txt
-grep "stem" output/$2.sl.txt | wc -l >> output/$2.stats.txt
-echo -n "nosl:" >> output/$2.stats.txt
-grep "nosl" output/$2.sl.txt | wc -l >> output/$2.stats.txt
+grep "s" output/$2.str.txt | wc -l >> output/$2.stats.txt
+echo -n "free:" >> output/$2.stats.txt
+grep -E "f|t" output/$2.str.txt | wc -l >> output/$2.stats.txt
--- structure_bias.pl
+++ structure_bias.pl
@@ -37,51 +37,20 @@ while (<FASTA>) {
 }
 close FASTA;
 
+my $fold = undef;
 my $str = undef;
 my $mfe = undef;
+my $est = undef;
+my $type = undef;
 foreach my $seq_id (@seq_id) {
 	# Predict RNA secondary structure
-	$str = `echo $seq{$seq_id} | RNAfold --noPS`;
+	$fold = `echo $seq{$seq_id} | RNAfold --noPS`;
 	# If a valid RNAfold out found
-	if ($str =~ /\(\.+\)\S*\s+\(\s*(\S+)\)/) {
-		# Capture MFE
-		$mfe = $1;
-		# If edit site locate on an unmatched region
-		if ($str =~ /\S+\n([\.\(\)]{$flank})\.([\.\(\)]{$flank})\s+\(\s*\S+\)/) {
-			# Caputre left and right parts respectively
-			my $left = $1;
-			my $right = $2;
-			# Remove dots
-			$left =~ s/\.//g;
-			$right =~ s/\.//g;
-			# If one of them is empty
-			if (length($left) == 0 or length($right) == 0) {
-				print "$seq_id\tfree\t$mfe\n";
-			} else {
-				# Extract the last and first matches
-				my $l = substr($left, length($left) - 1, 1);
-				my $r = substr($right, 1, 1);
-				# Judge structure: loop/bulge/free
-				if ($l eq "("  && $r eq "(")  {
-					print "$seq_id\tbulge\t$mfe\n";
-				} elsif ($l eq ")" && $r eq ")") {
-					print "$seq_id\tbulge\t$mfe\n";
-				} elsif ($l eq "(" && $r eq ")") {
-					print "$seq_id\tloop\t$mfe\n";
-				} elsif ($l eq ")" && $r eq "(") {
-					print "$seq_id\tfree\t$mfe\n";
-				} else {
-					warn "Unknown RNA secondary structure found in $seq_id!\n";
-				}
-			}
-		# If edit site locate on a matched region
-		} else {
-			print "$seq_id\tstem\t$mfe\n";
-		}
-	# If no stem-loop found
-	} else {
-		$str =~ /\s+\(\s*(\S+)\)/;
-		$mfe = $1;
-		print "$seq_id\tnosl\t$mfe\n";
+	if ($fold =~ /\S+\n(\S+)\s+\(\s*(\S+)\)/) {
+		$str = $1;
+		$mfe = $2;
+		$est = `echo "$str" | python dotbracket_to_element_string.py -`;
+		$type = substr($est, $flank, 1);
+		print "$seq_id\t$type\t$mfe\n";
 	}
 }
